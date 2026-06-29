28d0660fff8c755371e4e03cb1f48d080df4f1ff
ser45g <sergey1pes@gmail.com>
2026-06-29 16:24:57 +0300

    Fixed endpoint prefixes

--- AspNetCoreMongoApi/Helpers/EndpointRoutes.cs
+++ AspNetCoreMongoApi/Helpers/EndpointRoutes.cs
@@ -4,7 +4,7 @@ namespace AspNetCoreMongoApi.Helpers
 {
     public static class EndpointRoutes
     {
-        public static readonly string WeatherForecast = "/weather-forecast";
-        public static readonly string Todos = "/todo";
+        public static readonly string WeatherForecast = "/weather-forecasts";
+        public static readonly string Todos = "/todos";
     }
 }
